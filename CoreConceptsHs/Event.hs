b660d2c3cf81bc530c48a268365f5eb39f57b1be
fenja <kollasch@uni-bremen.de>
2017-07-04 02:06:30 +0200

    Remodeled Events since the current version on master is not compiling

--- CoreConceptsHs/Event.hs
+++ CoreConceptsHs/Event.hs
@@ -17,16 +17,49 @@ import Time
 -- the class of all event types
 -- Eq captures identity
 -- events are bounded in time, but do not need an explicit boundary
+{-
 class Eq (event time) => EVENTS event time where
-	bounds :: event time -> Interval time
+    bounds :: event time -> Interval time
 
 data InstantEvent time = InstantEvent (Instant time) deriving (Eq, Show)
 
 instance (Eq time, Ord time) => EVENTS InstantEvent time where
- 	bounds (InstantEvent (Instant t1 trs1)) = Interval (Instant t1 trs1) (Instant t1 trs1)
+    bounds (InstantEvent (Instant t1 trs1)) = Interval (Instant t1 trs1) (Instant t1 trs1)
 
 -- TESTS
 ie1, ie2 :: InstantEvent Int
 ie1 = InstantEvent i1
 ie2 = InstantEvent i2
 et1 = bounds ie1
+    -}
+
+class Eq event => EVENTS event where
+    bounds :: event -> Period
+    same :: event -> event -> Bool
+    during :: event -> event -> Bool
+    before :: event -> event -> Bool
+    after :: event -> event -> Bool
+    overlap :: event -> event -> Bool
+
+data InstantEvent = InstantEvent Instant deriving (Eq, Show)
+
+instance EVENTS InstantEvent where
+    bounds (InstantEvent time) = (time, time)
+    same e1 e2 = e1 == e2
+    during (InstantEvent t1) (InstantEvent t2) = t1 == t2
+    before (InstantEvent t1) (InstantEvent t2) = t1 < t2
+    after (InstantEvent t1) (InstantEvent t2) = t1 > t2
+    overlap = during -- just in the case of an instant event
+
+data IntervalEvent = IntervalEvent Instant Instant deriving (Eq, Show)
+
+instance EVENTS IntervalEvent where
+    bounds (IntervalEvent s e) = (s, e)
+    same e1 e2 = e1 == e2
+    during (IntervalEvent s1 e1) (IntervalEvent s2 e2) = s1 > s2 && e1 < e2
+    before (IntervalEvent s1 e1) (IntervalEvent s2 e2) = e1 <= s2
+    after (IntervalEvent s1 e1) (IntervalEvent s2 e2) = e2 <= s1
+    overlap (IntervalEvent s1 e1) (IntervalEvent s2 e2) = s2 <= e1 && e1 <= e2 || s1 <= e2 && e2 <= e1
+
+e = IntervalEvent (timestamp (date 2017 07 03) (time 16 57 00)) (timestamp (date 2017 07 03) (time 18 00 00))
+
